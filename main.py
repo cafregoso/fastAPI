216acd9acdfa34e06a18f9142d7f9b8bd4e0708c
Carlos Alvarez <car.alv2804@gmail.com>
2021-12-08 11:14:11 -0600

    validation-models

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Pythom
 from typing import Optional
+from enum import Enum
 
 # Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 # fastAPI
 from fastapi import FastAPI, Body, Query, Path
@@ -12,17 +13,36 @@ app = FastAPI()
 
 # Models
 
+class HairColor(Enum):
+    white = 'white'
+    brown = 'brown'
+    black = 'black'
+    blonde = 'blonde'
+    red = 'red'
+
 class Location(BaseModel):
     city: str
     state: str
     country: str
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None # parametro opcional, si no se incluye se manda None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+    )
+    hair_color: Optional[HairColor] = Field(default=None) # parametro opcional, si no se incluye se manda None
+    is_married: Optional[bool] = Field(default=None)
 
 @app.get("/")
 def home():
@@ -77,4 +97,8 @@ def update_person(
     person: Person = Body(...),
     location: Location = Body(...),
 ):
-    return person
+    results = person.dict()
+    results.update(location.dict())
+    return results
+
+    
