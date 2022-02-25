bc87ced521cea3d16fa6ed82890b5aad319ae479
Carlos Alvarez <cafregoso@outlook.com>
2022-02-25 10:49:46 -0600

    deprecate

--- main.py
+++ main.py
@@ -120,6 +120,7 @@ def create_person(person: Person = Body(...)): # el operador '...' indica que el
     path='/person/detail',
     status_code=status.HTTP_200_OK,
     tags=['Persons'],
+    deprecated=True
 )
 def show_person(
     name: Optional[str] = Query(
